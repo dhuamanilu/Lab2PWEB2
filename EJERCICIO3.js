8e53e1fe423156765be1c2499b5d46408e808495
Diego Huamani <dhuamanilu@unsa.edu.pe>
2022-05-13 09:31:54 -0500

    haciendo el 2do ejercicio y empezando el 3ero

--- EJERCICIO3.js
+++ EJERCICIO3.js
@@ -1,35 +1,4 @@
 
-//RESUELTO 1
-function arrayGenerator (n,min,max){
-	let a=[];
-	for(let x=0;x<n;x++){
-		a.push(Math.ceil(Math.random()*(max-min)+min));
-	}
-	return a;
-}
-//RESUELTO 1 CON MAP
-function arrayGeneratorMap (n,min,max){
-	let a=Array(n).fill(0);
-	let ArrayMap=a.map(function (min,max){return Math.ceil(Math.random()*(max-min)+min)});
-	return ArrayMap;
-}
-//RESUELTO 2
-function votesCounting(votes){
-	let counting=Array(1000).fill(0)
-	for(idx in votes){
-		counting[votes[idx]]++
-	}
-	let maxVotes=-100;
-	let winnerID=0;
-	for(id in counting){
-		if(counting[id]>maxVotes){
-			maxVotes=counting[id];
-			winnerID=id;
-		}
-	}
-	return {id : winnerID, votes: maxVotes}
-}
-
 //PROPUESTO1 
 function queDiaEs(numero){
 	numero%=7;
@@ -57,11 +26,11 @@ function queDiaEs(numero){
 }
  //PROPUESTO2
 document.addEventListener('DOMContentLoaded',function(){
-	const texto=document.querySelector("#text");
+	const texto=document.querySelector("#texto");
 	const button=document.querySelector("#enviar");
 	button.disabled=true;
-	name.onkeyup=()=> {
-		if(name.value.length>0){
+	texto.onkeyup=()=> {
+		if(texto.value.length>0){
 			button.disabled=false;
 		}
 		else{
